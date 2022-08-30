b7235a52f8da983d685a1484fa4b0beea7a19b1f
douliyoutang <798507702@qq.com>
2022-08-30 17:04:44 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,9 +1,28 @@
 // 每次调用 $.get() $.post() $.ajax()的时候
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中 可以拿到我们给ajax提供的配置对象
-$.ajaxPrefilter(function(options){
+$.ajaxPrefilter(function (options) {
     // console.log(options.url)
     // 在真正发起ajax请求之前 统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url
     console.log(options.url)
+
+    // 统一为有权限的接口 设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 全局统一挂载 complete 回调函数
+    options.complete = function (res) {
+        // console.log('执行了 complete 回调')
+        // console.log(res)
+        // 在 complete 回调函数中 可以使用 res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 || res.responseJSON.message === '身份认证失败！') {
+            // 1.强制清空token
+            localStorage.removeItem('token')
+            // 2.强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
